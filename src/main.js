8da0e0673f2b7fe70fcbbe8da6525d524bd307b4
Carl Olson <colson10@gmail.com>
2018-08-10 14:26:20 -0700

    Added redux thunk, profile actions and profile reducer

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@
 
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './components/app';
+import reducer from './reducers/index';
+import thunk from './lib/redux-thunk';
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
 const app = document.createElement('div');
 document.body.appendChild(app);
-render(<App/>, app);
+render(<Provider store={ store }><App/></Provider>, app);
---
+++ src/reducers/profile.js
@@ -0,0 +1,17 @@
+const D23_001 = 'Profile is required';
+
+const validateProfile = (profile) => {
+  if (!profile) throw new Error(D23_001);
+};
+
+export default (state = null, { type, payload }) => {
+  switch (type) {
+    case 'CLIENT_PROFILE_SET':
+      validateProfile(payload);
+      return payload;
+    case 'TOKEN_REMOVE':
+      return null;
+    default:
+      return state;
+  }
+};
