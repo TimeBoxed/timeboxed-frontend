729e9e93d59319938d4b38dc3da09f59c5e2ae08
Carl Olson <colson10@gmail.com>
2018-08-15 14:08:43 -0700

    Preferences fetch added to preferences view

--- src/components/preferences/preferences.js
+++ src/components/preferences/preferences.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import * as profileActions from '../../actions/profile';
+import * as preferencesActions from '../../actions/preferences';
 
 class Preferences extends React.Component {
   constructor(props) {
@@ -14,6 +15,9 @@ class Preferences extends React.Component {
   componentDidMount() {
     if (this.props.loggedIn) {
       return this.props.pFetchUserProfile()
+        .then(() => {
+          return this.props.pFetchUserPreferences();
+        })
         .then(() => {
           const updatedCalendars = [];
           this.props.profile.calendars.forEach(cal => (
@@ -36,6 +40,7 @@ class Preferences extends React.Component {
 
     return (
       <div>
+        {}
         Select the calendar that you would like to use:
         {
           this.state.calendars.length > 0 ? calendarDropdownJSX : 'You have no calendars'
@@ -49,15 +54,19 @@ Preferences.propTypes = {
   profile: PropTypes.object,
   loggedIn: PropTypes.bool,
   pFetchUserProfile: PropTypes.func,
+  preferences: PropTypes.object,
+  pFetchUserPreferences: PropTypes.func,
 };
 
 const mapStateToProps = state => ({
   profile: state.profile,
   loggedIn: !!state.token,
+  preferences: state.preferences,
 });
 
 const mapDispatchToProps = dispatch => ({
   pFetchUserProfile: () => dispatch(profileActions.profileFetchRequest()),
+  pFetchUserPreferences: () => dispatch(preferencesActions.preferencesFetchRequest()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Preferences);
