43e0d0a0bd55be61ea214b0537dc0d2d818b67ee
Carl Olson <colson10@gmail.com>
2018-08-21 17:44:56 -0700

    small fix on preferences, and added logo to assets

--- src/components/material-ui/menu-app-bar.js
+++ src/components/material-ui/menu-app-bar.js
@@ -13,6 +13,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import * as authActions from '../../actions/auth';
 import ROUTES from '../../routes';
+import Logo from '../../assets/logo/logo-white.svg';
 
 const styles = {
   root: {
@@ -20,10 +21,24 @@ const styles = {
   },
   flex: {
     flexGrow: 1,
+    maxWidth: '100px',
   },
   menuButton: {
     marginLeft: -12,
     marginRight: 20,
+  },
+  logo: {
+    maxHeight: '3em',
+    maxWidth: '3em',
+  },
+  logoContainer: {
+    width: '100%',
+    verticalAlign: 'middle',
+
+  },
+  logoText: {
+    display: 'inline-block',
+    height: '100%',
   },
 };
 
@@ -62,9 +77,14 @@ class MenuAppBar extends React.Component {
             <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
               <MenuIcon />
             </IconButton>
-            <Typography variant="title" color="inherit" className={classes.flex}>
-              TimeBoxed
-            </Typography>
+            <div className={classes.logoContainer}>
+              <Logo className={classes.logo} style={{ display: 'inline-block' }}/>
+              <div className={classes.logoText}>
+                <Typography variant="title" color="inherit" className={classes.flex}>
+                  TimeBoxed
+                </Typography>
+              </div>
+            </div>
             {auth && (
               <div>
                 <IconButton
