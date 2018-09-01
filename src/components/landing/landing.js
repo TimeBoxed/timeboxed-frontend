cae621378dc38acb3ec33d7f1a4bac3e832ebe72
Carl Olson <colson10@gmail.com>
2018-09-01 12:00:04 -0700

    added text next to logo on landing page

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -8,7 +8,12 @@ class Landing extends React.Component {
   render() {
     return (
     <div className='landing-page-div'>
-      <Logo />
+      <div className='landing-page-logo-div'>
+        <Logo />
+        <div className='logo-text'>
+          <h2>TimeBoxed</h2>
+        </div>
+      </div>
       <a href={`https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${API_URL}/oauth/signin&scope=openid%20profile%20email&client_id=30945685942-2peaeakg0megqic4lp8d4ffu79p520lm.apps.googleusercontent.com&prompt=consent&response_type=code`}>
       <button id='sign-in-button'><GoogleLogo id='googleLogo'/><p>Sign in with Google</p></button>
       </a>
