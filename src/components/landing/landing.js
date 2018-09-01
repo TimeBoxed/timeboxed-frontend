1828077e072fabd953f39c87a8dcd2e864c8b171
Carl Olson <colson10@gmail.com>
2018-09-01 11:31:26 -0700

    more styling on landing page and new account link added

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -9,9 +9,12 @@ class Landing extends React.Component {
     return (
     <div className='landing-page-div'>
       <Logo />
-      <a href={`https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${API_URL}/welcome&scope=profile%20email%20https://www.googleapis.com/auth/calendar&prompt=consent&response_type=code&client_id=30945685942-2peaeakg0megqic4lp8d4ffu79p520lm.apps.googleusercontent.com`}>
-      <button id='sign-in-button'><GoogleLogo id='googleLogo'/><p>SIGN IN WITH GOOGLE</p></button>
+      <a href={`https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${API_URL}/oauth/signin&scope=openid%20profile%20email&client_id=30945685942-2peaeakg0megqic4lp8d4ffu79p520lm.apps.googleusercontent.com&prompt=consent&response_type=code`}>
+      <button id='sign-in-button'><GoogleLogo id='googleLogo'/><p>Sign in with Google</p></button>
       </a>
+      <div className='new-account-link'>
+        <a href={`https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${API_URL}/welcome&scope=profile%20email%20https://www.googleapis.com/auth/calendar&prompt=consent&response_type=code&client_id=30945685942-2peaeakg0megqic4lp8d4ffu79p520lm.apps.googleusercontent.com`}><h4>CREATE NEW ACCOUNT</h4></a>
+      </div>
     </div>
     );
   }
