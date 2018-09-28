0b19cb24be50cba61875f94fa17785aab52331b0
Sarah Geyer <sjgeyer@comcast.net>
2018-09-27 20:47:39 -0700

    remove styles on snackbar for now because I can't get them to look good

--- src/components/material-ui/snackbar.js
+++ src/components/material-ui/snackbar.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { withStyles } from '@material-ui/core/styles';
 import Snackbar from '@material-ui/core/Snackbar';
 import Slide from '@material-ui/core/Slide';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import ErrorIcon from '@material-ui/icons/Error';
-
-const styles = {
-  error: {
-    backgroundColor: 'red',
-  },
-  success: {
-    backgroundColor: 'green',
-  },
-};
 
 const TransitionUp = (props) => {
   return <Slide {...props} direction="up" />;
@@ -34,13 +22,6 @@ class DirectionSnackbar extends React.Component {
       open,
     } = this.props;
 
-    const messageJSX = (
-      <div className={classes[type]}>
-        { type === 'error' ? <ErrorIcon/> : <CheckCircleIcon/> }
-        <span id="message-id">{message}</span>
-      </div>
-    );
-
     return (
       <div>
         <Snackbar
@@ -49,7 +30,8 @@ class DirectionSnackbar extends React.Component {
           ContentProps={{
             'aria-describedby': 'message-id',
           }}
-          message={messageJSX}
+          message={<span id="message-id">{message}</span>}
+          className={classes[type]}
         />
       </div>
     );
@@ -75,4 +57,4 @@ const mapStateToProps = state => ({
   type: state.ui.snackbar.sbType,
 });
 
-export default connect(mapStateToProps)(withStyles(styles)(DirectionSnackbar));
+export default connect(mapStateToProps)(DirectionSnackbar);
