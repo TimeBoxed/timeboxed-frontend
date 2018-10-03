aab566890b3eb2f4da005c57eadb9df5868e8a70
Sarah Geyer <sjgeyer@comcast.net>
2018-10-02 19:31:36 -0700

    remove unnecessary case from token reducer

--- src/reducers/token.js
+++ src/reducers/token.js
@@ -3,10 +3,8 @@ import { fetchCookie } from '../utils/cookie';
 const token = fetchCookie('GT1234567890');
 const initialState = token || null;
 
-export default (state = initialState, { type, payload }) => {
+export default (state = initialState, { type }) => {
   switch (type) {
-    case 'TOKEN_SET':
-      return payload;
     case 'TOKEN_REMOVE':
       return null;
     default:
